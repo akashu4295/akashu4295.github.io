a28aff1f1c8dc355486f0c193267c70e51bb855a
Akash Unnikrishnan <82869237+akashu4295@users.noreply.github.com>
2024-12-24 16:51:10 +0530

    Update tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                gray: {
+                    1000: '#000000', // Custom black
+                    400: '#808080',  // Custom grey
+                },
+            },
+        },
+    },
+};
